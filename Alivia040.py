deb562e4a5f5268948b375a35320a16f818fa1f7
alivia07 <alivianazwa007@gmail.com>
2026-06-13 21:16:47 +0700

    Update Alivia040.py

--- Alivia040.py
+++ Alivia040.py
@@ -1,4 +1,4 @@
-def hitung_determinan_3x3(m):
+def hitung_determinan_3x3(matriks):
     return (m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) -
             m[0][1] * (m[1][0] * m[2][2] - m[1][2] * m[2][0]) +
             m[0][2] * (m[1][0] * m[2][1] - m[1][1] * m[2][0]))
